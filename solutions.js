d8869d9efef89b61db5584a0b2aa45458301a5ea
bartwiharto <bartwiharto@gmail.com>
2018-03-22 18:36:32 -0600

    Commit 3 - Digit Sum.

--- solutions.js
+++ solutions.js
@@ -4,12 +4,37 @@
 //3. Within a function, return is will return the result of a function so that it can be use for something else. 
 // Console.log will display the result of a function inside the console.
 
-const checkPalindrome = (checkWord) => {
-		if (checkWord === checkWord.split('').reverse().join('')) {
-				return(true);
-		} else {
-			return(false);
-		}
+// const checkPalindrome = (checkWord) => {
+// 		if (checkWord === checkWord.split('').reverse().join('')) {
+// 				return(true);
+// 		} else {
+// 				return(false);
+// 		}
+// }
+
+// checkPalindrome("boolean");
+
+//Digit Sum: (Can only do up to 2 digits)
+
+const sumDigits = (number) => {
+		let i = number.toString().split('');
+		return (parseInt(i[0]) + parseInt(i[1]));
 }
 
-checkPalindrome("boolean");
+sumDigits(49);
+
+
+//Pythagoras:
+
+// let sideC;
+
+// const calculateSide = (sideA, sideB) => {
+
+// }
+
+
+
+
+
+
+
